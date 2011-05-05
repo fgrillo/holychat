92c10300ac460fa602dd13418e238f6ec1835d1f
Filipe Grillo <filipe.grillo@gmail.com>
2011-05-05 15:45:07 -0300

    Changed to ajax (a lot of work)

--- benchmark.js
+++ benchmark.js
@@ -1,32 +1,121 @@
 var sys = require('sys');
-var WebSocket = require('websocket-client').WebSocket;
 var utils = require('./utils.js');
+var http = require('http');
+var url = require('url');
 
-var testUserEmail = 'benchmark@gmail.com';
-var ws = new WebSocket('ws://localhost:8080/socket.io/websocket');
-//var ws = new WebSocket('ws://173.203.127.245:8080/socket.io/websocket');
+var host = '127.0.0.1';
+var port = 4567;
+var users = 0;
 
-var joined = false;
+var randomMessage = "Lorem+Ipsum+is+simply+dummy+text+of+the+printing+and+typesetting+industry.+Lorem+Ipsum+has+been+the+industry's";
 
-ws.onmessage = function(message) {
-  var payload = utils.decode(message.data)[0];
+function generateRandomMessage() {
+  var extraSize = parseInt(Math.random() * 100);
+  return "Benchmark+Message:+" + randomMessage.substring(0, extraSize);
+}
 
-  if (joined == false) {
-    console.log('Sending join message');
-    ws.send(utils.encode({action: 'join', email: testUserEmail}));
 
-    joined = true;
+function user() {
 
-  } else if (payload.substr(0, 3) === '~j~') {
-    var data = JSON.parse(payload.substr(3));
-
-    if (data.action === 'join' && data.email === testUserEmail) {
-      console.log('Sending bechmark messages');
-
-      for(var i=0; i<100; i++) {
-        ws.send(utils.encode({action: 'message', message: 'Benchmark message ' + i}));
-      }
+    function joinAction() {
+        var join_req = http.request({host: host, port: port, method: 'post', path: '/join?email=' + testUserEmail}, function(res) {
+            // console.log('STATUS: ' + res.statusCode);
+            // console.log('HEADERS: ' + JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                var result = JSON.parse(chunk);
+                console.log('BODY: ' + result[1]);
+            });
+        });
+        join_req.end();
     }
-  }
+    function getMessages() {
+        var get_req = http.request({host: host, port: port, method: 'get', path: '/get-messages?current=' + currentMessage + '&email=' + testUserEmail}, function(res) {
+            // console.log('STATUS: ' + res.statusCode);
+            // console.log('HEADERS: ' + JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                // console.log('BODY: ' + chunk);
+            });
+        });
+        get_req.end();
+    }
 
+    function sendMessage() {
+        message = generateRandomMessage();
+
+        var send_req = http.request({host: host, port: port, method: 'post', path: '/message?email=' + testUserEmail + '&msg=' + message}, function(res) {
+            // console.log('STATUS: ' + res.statusCode);
+            // console.log('HEADERS: ' + JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                // console.log('BODY: ' + chunk);
+            });
+        });
+        
+        send_req.end();
+    }
+
+
+    function leaveAction() {
+        var leave_req = http.request({host: host, port: port, method: 'post', path: '/leave?email=' + testUserEmail}, function(res) {
+            // console.log('STATUS: ' + res.statusCode);
+            // console.log('HEADERS: ' + JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                // console.log('BODY: ' + chunk);
+            });
+        });
+        leave_req.end();
+    }
+
+
+  var testUserEmail = null;
+  
+  var tss = [];
+
+  var testUserEmail = 'user' + ++users + '@benchmark.com';
+  var currentMessage = 0;
+
+  joinAction();
+
+
+  
+  // ws.onmessage = function(message) {
+  //   var payload = utils.decode(message.data)[0];
+  // 
+  //   if (joined == false) {
+  //     testUserEmail = 'user' + ++users + '@benchamrk.com';
+  //     ws.send(utils.encode({action: 'join', email: testUserEmail}));
+  // 
+  //     joined = true;
+  // 
+  //   } else if (payload.substr(0, 3) === '~j~') {
+  //     var data = JSON.parse(payload.substr(3));
+  // 
+  //     if (data.action === 'join' && data.email === testUserEmail) {
+  //       setInterval(function() {
+  //         tss.push(new Date());
+  //         ws.send(utils.encode({action: 'message', message: generateRandomMessage()}));
+  //       }, 100);
+  // 
+  //     } else if (data.action === 'message' && data.email === testUserEmail) { 
+  //         var d = new Date();
+  //         var ts = d.getDay() + '/' + d.getMonth() + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + ':' + d.getMilliseconds();
+
+  //         var sended = tss.shift();
+  //         var dt = d.getTime() - sended.getTime();
+
+  //         console.log(ts + ',' + users + ',' + payload.length + ',' + dt);
+  //     }
+  //   } else if (payload.substr(0, 3) === '~h~') {
+  //     ws.send(utils.encode('~h~' + ++heartBeats));
+  //   }
+  // }
+}
+
+for(var i=1; i<=1; i++) {
+  setTimeout(function() {
+    user();
+  }, i * 1100);
 }
--- holychat.js
+++ holychat.js
@@ -2,6 +2,9 @@ var express = require('express');
 var jade = require('jade');
 var io = require('socket.io');
 
+var user_list = new Array();
+var msg_list = new Array();
+
 // Express - Configure App
 var app = express.createServer();
 
@@ -29,53 +32,93 @@ app.get('/', function(req, res) {
 
 });
 
+app.post('/join', function(req, res) {
+    var email = req.param('email');
+
+    if (email != '' && search_user(email) == -1) {
+        user_list.push([email, 0]);
+        for (i = 0 ; i < user_list.length ; i++) {
+            if (email != user_list[i][0]) {
+                user_list[i][1] = 1;
+            }
+        }
+        console.log('number of users: ' + user_list.length)
+        res.send([user_list, msg_list.length]);
+        msg_list.push(['', 'Usuário <b>' + email.substr(0, email.indexOf("@")) + '</b> entrou no chat']);
+    }
+
+});
+
+app.post('/leave', function(req, res) {
+    var email = req.param('email');
+
+    console.log('email: ' + email)
+
+    for (i = 0 ; i < user_list.length ; i++) {
+        if (user_list[i][0] === email) {
+            user_list.splice(i, 1);
+            msg_list.push(['', 'Usuário <b>' + email + '</b> saiu do chat']);
+        } else {
+            user_list[i][1] = 1;
+        }
+    }
+
+    console.log('user logged out! number of users: ' + user_list.length);
+
+    res.send('success');
+});
+
+app.post('/message', function(req, res) {
+
+    var email = req.param('email');
+    var message = req.param('msg')
+
+    if (message != '') {
+        msg_list.push([email, message]);
+        res.send('success');
+        console.log('message saved! new lenght: ' + msg_list.length);
+    } else {
+        res.send('error');
+    }
+
+});
+
+app.get('/get-messages', function(req, res) {
+    
+    var current = req.param('current');
+    var email = req.param('email');
+
+    var response = new Array();
+
+    var index = search_user(email);
+
+    if (index != -1 && user_list[index][1] == 1) {
+        response.push(user_list);
+        user_list[i][1] = 0;
+    } else {
+        response.push(new Array());
+    }
+
+    console.log('current: ' + current + ' lenght: ' + msg_list.length);
+    if (msg_list.length > current) {
+        response.push(msg_list.slice(current));
+        response.push(msg_list.length);
+        res.send(response);
+    } else {
+        res.send(null);
+    }
+});
+
+function search_user(email) {
+    for (i = 0 ; i < user_list.length ; i++) {
+        if (user_list[i][0] == email) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 // Initialize http server
-app.listen(8080);
+app.listen(4567);
 console.log("Holy Chat Started on port %d", app.address().port);
 
-// Initialize Socket.IO
-var socket = io.listen(app);
-
-socket.on('connection', function(client) {
-
-  client.on('message', function(data) {
-    console.log('Message received');
-    console.log(data);
-
-    if (data.action === 'message') {
-      // set client email
-      data.email = client.email;
-
-      // broadcast message
-      socket.broadcast(data);
-
-    } else if (data.action === 'join') {
-      // set client email
-      client.email = data.email;
-
-      // set that this is a new user
-      data.newUser = true;
-
-      // broadcast joined message
-      socket.broadcast(data);
-
-      // send all other clients to the new user
-      for(var i in socket.clients) {
-        var c = socket.clients[i];
-
-        // Don't send duplicated message
-        if (c.email === client.email) return;
-
-        client.send({action: 'join', email: c.email, newUser: false});
-      }
-    }
-  });
-
-  client.on('disconnect', function() {
-    var data = {};
-    data.action = 'leave';
-    data.email = client.email;
-
-    client.broadcast(data);
-  });
-});
