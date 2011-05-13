41e161bee986ff0de96a02e6c48f9ea1db6282d9
Filipe Grillo <filipe.grillo@gmail.com>
2011-05-12 22:27:16 -0300

    - Commenting code - Moved result files to result_data folder

--- holychat.js
+++ holychat.js
@@ -1,3 +1,4 @@
+// Include required modules
 var express = require('express');
 var jade = require('jade');
 var io = require('socket.io');
@@ -8,8 +9,10 @@ var msg_list = new Array();
 var allMessages = 0;
 var dtMessages = 0;
 
+// Command to get system usage information 
 var getCpuCommand = "ps -p " + process.pid + " -u | grep " + process.pid;
 
+// Function to print the server log on the standard output
 function printLog() {
   var child = exec(getCpuCommand, function(error, stdout, stderr) {
        var d = new Date();
@@ -40,8 +43,6 @@ app.configure(function() {
 // Express - Routes
 app.get('/', function(req, res) {
 
-  //console.log("Serving chat app");
-
   res.render('chat', {
     layout: 'chatLayout',
     title: 'Holy Chat!',
@@ -52,59 +53,71 @@ app.get('/', function(req, res) {
 
 });
 
+// Path for users to join the chat room
 app.post('/join', function(req, res) {
     var email = req.param('email');
 
     if (email != '' && search_user(email) == -1) {
+
+        // Add the new user to the user array an mark the other users to update
+        // their user list.
         user_list.push([email, 0]);
         for (i = 0 ; i < user_list.length ; i++) {
             if (email != user_list[i][0]) {
                 user_list[i][1] = 1;
             }
         }
-        //console.log('number of users: ' + user_list.length)
         res.send([user_list, msg_list.length]);
+
+        // Add a message telling to the user that a new user joined the chat room
         msg_list.push(['', 'Usuário <b>' + email.substr(0, email.indexOf("@")) + '</b> entrou no chat']);
     }
 
 });
 
+
+// Path for users to leav the chat room
 app.post('/leave', function(req, res) {
     var email = req.param('email');
 
-    //console.log('email: ' + email)
-
     for (i = 0 ; i < user_list.length ; i++) {
+        // remove the user from users array and add a message to all users
         if (user_list[i][0] === email) {
             user_list.splice(i, 1);
             msg_list.push(['', 'Usuário <b>' + email + '</b> saiu do chat']);
         } else {
+            // Mark all other users to update their user list.
             user_list[i][1] = 1;
         }
     }
-
-    //console.log('user logged out! number of users: ' + user_list.length);
-
+    
+    // respond success to the user browser.
     res.send('success');
 });
 
+
+// Path for users to send a message to the chat room
 app.post('/message', function(req, res) {
 
     var email = req.param('email');
     var message = req.param('msg')
 
     if (message != '') {
+        // Add message to the message array and update some log variables
         msg_list.push([email, message]);
         dtMessages++;
         allMessages++;
+
+        // respond success to the request
         res.send('success');
-        //console.log('message saved! new lenght: ' + msg_list.length);
     } else {
+        // if there is no content responde error to the request
         res.send('error');
     }
 
 });
 
+// Path for users to get the lastest messages from the server
 app.get('/get-messages', function(req, res) {
     
     var current = req.param('current');
@@ -115,25 +128,29 @@ app.get('/get-messages', function(req, res) {
     var index = search_user(email);
 
     if (index != -1 && user_list[index][1] == 1) {
+        // If the this user's user list is outdated, add it to the response
         response.push(user_list);
         user_list[i][1] = 0;
     } else {
         response.push(new Array());
     }
 
+    // Increment log variables
     dtMessages++;
     allMessages++;
 
-    //console.log('current: ' + current + ' lenght: ' + msg_list.length);
     if (msg_list.length > current) {
+        // If there are new messages
         response.push(msg_list.slice(current));
         response.push(msg_list.length);
         res.send(response);
     } else {
+        // If there is no new message send an empty response
         res.send(null);
     }
 });
 
+// Utility function for searching the user on the user array
 function search_user(email) {
     for (i = 0 ; i < user_list.length ; i++) {
         if (user_list[i][0] == email) {
@@ -145,9 +162,8 @@ function search_user(email) {
 
 // Initialize http server
 app.listen(4567);
-//console.log("Holy Chat Started on port %d", app.address().port);
 
-// Log cpu and memory utilization
+// Log CPU and memory usage and communication ammount
 setInterval(function() {
   printLog();
 }, 1000);
